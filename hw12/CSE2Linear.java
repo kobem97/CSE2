fd6feada0640fc849229f0d67c13af75d9b4561f
kobem97 <kjm219@lehigh.edu>
2016-04-22 02:46:05 +0000

    hw12

--- hw12/CSE2Linear.java
+++ hw12/CSE2Linear.java
@@ -1,107 +1,124 @@
 //Kobe Miller
-//hw12
-//CSE2Linear
-//April 18, 2016
+//HW12
+//4/18/2016
+//Asks user to input 15 numbers in order between 0 and 100
+//then ask user to search of int
+//Prints out how many times it took to find it
+//shuffles array and repeats
 
-//imports the scanner
-import java.util.Scanner; //Scanner is a new class
-import java.util.Arrays;  //Arrays is a new class
+import java.util.Scanner;
+import java.util.Arrays;
 
-public class CSE2Linear {
-       public static void main(String[] args) {
-            int numGrade = 15; //limits ho much user can input
-            Scanner gradeInput = new Scanner(System.in); //Declaration
-            try //as long as inputs are ints, program will run
-            {
-                int[] grades = new int[numGrade]; //import array
-                System.out.print("Enter in 15 final grades: "); //What the user sees
-                for(int i = 0; i < grades.length; i++) //Stores numbers for array
+public class CSE2Linear{
+       public static void main(String[] args){
+            Scanner myScanner = new Scanner(System.in);
+                int totalGrade = 15;
+                int grades[] = new int[totalGrade];
+                int temp = 0;
+                int i = 0;
+                int inter = 0;
+                System.out.print("Enter 15 int 0-100, in increasing order: ");
+                //Makes sure numbers are in increasing order
+                while(i < totalGrade){
+                    while (!myScanner.hasNextInt())
+                    {
+                    myScanner.next();
+                    System.out.println("invalid input. Please enter an integer.");
+                    }
+                    temp = myScanner.nextInt();
+                    if(temp < 0 || temp > 100)
+                    {
+                        System.out.println("Please enter an integer between 0 and 100.");
+                    }
+                    //Then stored
+                    else{
+                        grades[i] = temp;
+                        //if numbers are wrong, it prints this
+                        if(i > 0 && temp < grades[i-1])
+                        {
+                            System.out.println("The integers must be in increasing order.");
+                            i--;
+                        }
+                        i++;
+                    }
+                }
+                //Prints user input
+                System.out.println("User Input: ");
+                for(i = 0; i < grades.length; i++)
                 {
-                    grades[i] = gradeInput.nextInt();
-                    int finalGrade = grades[i]; //Makes it so if statement can check grades
-                    if (finalGrade<0 || finalGrade>100) //checks if its in bound
-                    {
-                        System.out.println("Invalid, must enter a number between 0 and 100");
-                        return;
-                    }
+                    System.out.print(grades[i] + " ");
                 }
-                System.out.println("Sort: ");
-                Arrays.sort(grades); //sorts the array
-                System.out.println(" "+ Arrays.toString(grades)); //prints array
-                System.out.println("Enter a grade to search for: ");
-                int gradeCheck = gradeInput.nextInt(); // new scanner
-                for (int i = 0; i < grades.length; i++){
-                    if (gradeCheck == grades[i]) //if user input grade is equal to the grade in array, it will run this
+                System.out.println("");
+                //Search array
+                System.out.print("Please enter an integer to be searched for 0 - 100: ");
+                while (!myScanner.hasNextInt())
+                {
+                    myScanner.next();
+                    System.out.println("Invalid input. Please enter an int.");
+                }
+                int target1 = myScanner.nextInt();
+                System.out.println("You entered " + target1);
+                int low = 0;
+                int high = grades.length - 1;
+                int mid = 0;
+                int counter = 0;
+                while (low <= high)
+                {
+                    mid = (low + high) / 2;
+                    if (grades[mid] > target1) 
                     {
-                        int it = i + 1; //
-                        System.out.println(gradeCheck + " was found in " + it + " iterations");  //found it! so we immediately exit the method
-                        System.out.println("Scrambled; ");
-                        for (i=0; i<grades.length; i++) {
-        	                //find a random member to swap with
-        	                int target = (int) (grades.length * Math.random()); //randomizes the order of array
-        	                //swap the values
-        	                int temp = grades[target]; //updates
+                        high = mid - 1;
+                    }
+                    else if (grades[mid] < target1) 
+                    {
+                        low = mid + 1;
+                    }
+                    counter++;    
+                }
+                if (target1 == grades[mid])
+                {
+                    System.out.print(target1 + " was found after " + counter + " iterations." );
+                }
+                else
+                {
+                    System.out.println(target1 + " was not found after " + counter + " iterations." );
+                }    
+                //Scrambles array
+                for (i=0; i<grades.length; i++)
+                {
+        	       int target = (int) (grades.length * Math.random());
+        	                inter = grades[target];
         	                grades[target] = grades[i];
-        	                grades[i] = temp; //updates
-                        }
-                        System.out.println(" "+ Arrays.toString(grades)); 
-                        System.out.println("Enter a grade to search for: ");
-                        gradeCheck = gradeInput.nextInt(); // user input to check for grade from randomized array
-                        for (i = 0; i < grades.length; i++){
-                            if (gradeCheck == grades[i])
-                            {
-                                it = i + 1;
-                                System.out.println(gradeCheck + " was found in " + it + " iterations");  //found it! so we immediately exit the method
-                                return;
-                            }
-                            if (i == 14 && gradeCheck != grades[i])
-                            {
-                                it = i + 1;
-                                System.out.println(gradeCheck + " was not found in " + it + " iterations");
-                                return;
-                            }
-                        }
-                    }   
-                    else if (i == 14 && gradeCheck != grades[i])
+        	                grades[i] = inter;
+                }
+                //Prints new array   
+                System.out.println("Scrambled: ");        
+                for(i = 0; i < grades.length; i++)
+                {
+                    System.out.print(grades[i] + " ");
+                }
+                System.out.println("");  
+                //Asks for int to be searched
+                System.out.print("Enter an integer to be searched for 0 - 100: ");
+                while (!myScanner.hasNextInt())
+                {
+                    myScanner.next();
+                    System.out.println("Invalid input. Please enter an int.");
+                }
+                    int target2 = myScanner.nextInt();
+                    System.out.println("You entered " + target2);
+                //Print found int 
+                for(i = 0; i < grades.length; i++)
+                {
+                    if(grades[i] == target2)
                     {
-                        int it = i + 1;
-                        System.out.println(gradeCheck + " was not found in " + it + " iterations");
-                        System.out.println("Scrambled; ");
-                        for (i=0; i<grades.length; i++) {
-            	            //find a random member to swap with
-            	            int target = (int) (grades.length * Math.random() );
-            	            //swap the values
-            	            int temp = grades[target];
-            	            grades[target] = grades[i];
-            	            grades[i] = temp;
-                        }
-                        System.out.println(" "+ Arrays.toString(grades));
-                        System.out.println("Enter a grade to search for: ");
-                        gradeCheck = gradeInput.nextInt();
-                        for (i = 0; i < grades.length; i++){
-                            if (gradeCheck == grades[i])
-                                {
-                                    it = i + 1;
-                                    System.out.println(gradeCheck + " was found in " + it + " iterations");  //found it! so we immediately exit the method
-                                    return;
-                            }
-                            if (i == 14 && gradeCheck != grades[i])
-                            {
-                                it = i + 1;
-                                System.out.println(gradeCheck + " was not found in " + it + " iterations");
-                                return;
-                            }
-                        }
+                        System.out.println(target2 + " was found after " + i + " iterations.");
+                        break;
+                    } 
+                    else if(i == 14)
+                    {
+                        System.out.println(target2 + " was not found after " + (i+1) + " iterations.");
                     }
                 }
-            }
-                    
-            catch(Exception e)
-            {
-                System.out.println("Invalid, must enter an int");
-                //black catch in order to continue to the print
-                
-            }
- 
-        }
-}
+    }//End of Main Method
+} //End of Class
